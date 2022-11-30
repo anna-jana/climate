6848e15d983f2286d4a7209e22c21bd153be7e63
Anna-Jana Riess <j.riess@wtnet.de>
2022-11-30 20:23:31 +0100

    plotting functions

--- dwd_data/analysis.py
+++ dwd_data/analysis.py
@@ -5,21 +5,19 @@ from mpl_toolkits.basemap import Basemap
 
 import data
 
-class Analysis:
-    def __init__(self, data):
-        self.mean_year = data.groupby(data.index.day_of_year).aggregate(np.mean)
-        self.years = data.groupby(data.index.year).aggregate([np.nanmin, np.nanmean, np.nanmax])
+# mean_year = data.groupby(data.index.day_of_year).aggregate(np.mean)
 
-def plot_station_positions(d):
+stations = data.load_stations()
+
+def plot_station_positions():
     m = Basemap(
         projection="lcc",
         resolution="i", # c: curde, l: low, i: intermediate (h: high, f: full but these are not installed
-        lat_0=0.5*(d.geoBreite.min() + d.geoBreite.max()),
-        lon_0=0.5*(d.geoLaenge.min() + d.geoLaenge.max()),
+        lat_0=0.5*(stations.geoBreite.min() + stations.geoBreite.max()),
+        lon_0=0.5*(stations.geoLaenge.min() + stations.geoLaenge.max()),
         width=1000_000,
         height=1000_000,
     )
-
     water_color = (0.3, 0.3, 0.7)
     m.drawcoastlines() # black lines for the coast
     m.drawmapboundary(fill_color=water_color) # the sea/ocean in blue
@@ -30,37 +28,38 @@ def plot_station_positions(d):
     m.drawparallels(parallels, labels=[True]*len(parallels))
     meridians = np.arange(4, 18, 2)
     m.drawmeridians(meridians, labels=[True]*len(meridians))
-
-    m.scatter(d.geoLaenge, d.geoBreite, color="black", marker="x", latlon=True)
-
+    m.scatter(stations.geoLaenge, stations.geoBreite, color="black", marker="x", latlon=True)
     plt.title("Positions of DWD weather stations", pad=20)
     plt.xlabel("Longitude")
     plt.ylabel("Latitude")
 
-stations = data.load_stations()
-
 def find_name_by_id(i):
     return stations.Stationsname[stations.Stations_id == i].iloc[0]
 
-def find_id_by_name(name):
+def find_id_by_name(name, nth=0):
     return stations.Stations_id[
             stations.Stationsname.str.lower().str.contains(name.lower())
-    ].iloc[0]
+    ].iloc[nth]
 
-def plot_mean_trend(d, name=None):
-    a = Analysis(d)
-    plt.figure(layout="constrained")
-    a.years.TMK.nanmean.plot()
-    plt.xlabel("year")
-    plt.ylabel("mean temperature in year [°C]")
-    plt.title(name)
+def search_by_name(name):
+    return stations.Stationsname[
+            stations.Stationsname.str.lower().str.contains(name.lower())
+    ]
 
-def plot_max_trend(d):
-    a = Analysis(d)
-    a.years.TXK.nanmax.plot()
-    plt.xlabel("year")
-    plt.ylabel("mean temperature in year [°C]")
-    plt.title(name)
+def plot_trends(d, name=""):
+    years = d.groupby(d.index.year).aggregate([np.nanmin, np.nanmean, np.nanmax])
+    fig, axs = plt.subplots(1, 3, layout="constrained")
+    years.TMK.nanmin.plot(ax=axs[0])
+    axs[0].set_xlabel("year")
+    axs[0].set_ylabel("temperature [°C]")
+    axs[0].set_title("min")
+    years.TMK.nanmean.plot(ax=axs[1])
+    axs[1].set_xlabel("year")
+    axs[1].set_title("mean")
+    years.TMK.nanmax.plot(ax=axs[2])
+    axs[2].set_xlabel("year")
+    axs[2].set_title("max")
+    fig.suptitle(name)
 
 def plot_climate_diagram(d, name=None):
     by_months = d.groupby(d.index.month)
@@ -68,7 +67,6 @@ def plot_climate_diagram(d, name=None):
     mean_ = by_months.aggregate(np.mean)
     max_  = by_months.aggregate(np.max)
     months = range(1, 12 + 1)
-
     plt.figure(layout="constrained")
     ax1 = plt.gca()
     ax2 = ax1.twinx()
@@ -83,8 +81,4 @@ def plot_climate_diagram(d, name=None):
     ax1.set_title(name)
     ax2.bar(months, mean_.RSK, alpha=0.7)
     ax2.set_ylabel("rainfall (height) [mm]")
-
-
-
-
 
